6d74b392da6e74d351119fcdcaaf16b22f28df28
alitalaat22 <elshaaerali16@gmail.com.com>
2024-02-13 19:28:52 +0200

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 10):
-	for nums in range(num+1, 10 ):
-		if num == 8 and nums == 9:
-			print("{:d}{:d}".format(num, nums))
-		else:
-			print("{:d}{:d}".format(num, nums), end=', ')
+    for nums in range(num+1, 10):
+        if num == 8 and nums == 9:
+            print("{:d}{:d}".format(num, nums))
+        else:
+            print("{:d}{:d}".format(num, nums), end=', ')
