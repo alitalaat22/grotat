7c3d3515a00492fd7cfb247fa7ea2183360741a3
alitalaat22 <elshaaerali16@gmail.com.com>
2024-02-13 13:30:31 +0200

    task 3 with continue

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alphabet in range(ord('a'), ord('{')):
     if alphabet == ord('q') or alphabet == ord('e'):
         continue
-    print('{:c}' .format(alphabet))
+    print('{:c}' .format(alphabet), end='')
